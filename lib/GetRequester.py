65e274ce8bd2b9cd417ce0e6d0551acf3fff4135
Ndolo Mercy <mercymwendend444@gmail.com>
2025-12-04 14:55:42 +0300

    Completed Lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,20 @@
 import requests
 import json
 
+
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        response.raise_for_status()
+        return response.content  # ← RETURN BYTES, not .text
 
     def load_json(self):
-        pass
+        return json.loads(self.get_response_body())
+    
+if __name__ == "__main__":
+    requester = GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+    data = requester.load_json()
